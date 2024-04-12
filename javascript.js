1b3f4c020104e86800f036fa355f25a8be192060
strgtscntst <66039310+strgtscntst@users.noreply.github.com>
2024-04-12 15:40:12 -0400

    add input validation

--- javascript.js
+++ javascript.js
@@ -3,33 +3,39 @@ const computerSelection = "";
 let selectionList = ["rock","paper","scissors"];
 
 function rockPaperScissors(playerSelection, computerSelection = getComputerChoice()){
-    console.log(computerSelection);
-    if (playerSelection === computerSelection){
-        return "draw"
-    }
-    if (playerSelection === "rock"){
-        if (computerSelection === "paper"){
-            return "loss"
+    playerSelection = playerSelection.toLowerCase();
+    if(selectionList.includes(playerSelection)){
+        console.log("The computer throws " + computerSelection);
+
+        if (playerSelection === computerSelection){
+            return "draw"
         }
-        if (computerSelection === "scissors"){
-            return "win"
+        if (playerSelection === "rock"){
+            if (computerSelection === "paper"){
+                return "loss"
+            }
+            if (computerSelection === "scissors"){
+                return "win"
+            }
         }
-    }
-    if (playerSelection === "paper"){
-        if (computerSelection === "scissors"){
-            return "loss"
+        if (playerSelection === "paper"){
+            if (computerSelection === "scissors"){
+                return "loss"
+            }
+            if (computerSelection === "rock"){
+                return "win"
+            }
         }
-        if (computerSelection === "rock"){
-            return "win"
+        if (playerSelection === "scissors"){
+            if (computerSelection === "rock"){
+                return "loss"
+            }
+            if (computerSelection === "paper"){
+                return "win"
+            }
         }
-    }
-    if (playerSelection === "scissors"){
-        if (computerSelection === "rock"){
-            return "loss"
-        }
-        if (computerSelection === "paper"){
-            return "win"
-        }
+    } else {
+        console.log("Inappropriate input");
     }
 }
 
@@ -41,5 +47,5 @@ function getComputerChoice(){
 
 //  Function to play 5 rounds
 function playGame(){
-    rockPaperScissors
+    console.log(rockPaperScissors(prompt()))
 }

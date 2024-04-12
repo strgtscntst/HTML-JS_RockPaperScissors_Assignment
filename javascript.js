2fcdad88141267ae75f7baad14ee01a74f37e5b6
strgtscntst <66039310+strgtscntst@users.noreply.github.com>
2024-04-12 14:35:07 -0400

    Add game pseudocode

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//  play a round of R-P-S, using passed argument as player input
+//  assign computer choice
+
+//      pick from rock, paper, or scissors
+//      if player=computer draw
+//      if playerRock
+//          if computerpaper - lose
+//          if computerscissor -win
+//      if playerPaper
+//          if computerScissor - lose
+//          if computerRock - win
+//      if playerScissor
+//          if computerRock - lose
+//          if computerPaper - win
+//  return result, outputting computer choice and win/loss/draw
+
+// function to play five games
+// - console.log results

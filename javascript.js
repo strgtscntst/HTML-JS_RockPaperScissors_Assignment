ab6f49e68529093b58e491c1b064372e256e3c6a
strgtscntst <66039310+strgtscntst@users.noreply.github.com>
2024-05-07 13:30:41 -0400

    add button functionality, results and score disp.

--- javascript.js
+++ javascript.js
@@ -1,25 +1,43 @@
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const resultsBox = document.querySelector("#results");
+const scoreBox = document.querySelector("#score");
+const compChoiceOutput = document.querySelector("#computerThrows");
+
+
 const playerSelection = "";
 const computerSelection = "";
 
 // SELECTION_LIST has extra "rock" to allow win condition to check against the values at index and index+1
 const SELECTION_LIST = ["rock","paper","scissors", "rock"];
 
+let wins = 0;
+let losses = 0;
+
 // Function to play a single round of Rock-Paper-Scissors
 function rockPaperScissors(playerSelection, computerSelection = getComputerChoice()){
     playerSelection = playerSelection.toLowerCase();
     if(SELECTION_LIST.includes(playerSelection)){
-        console.log("The computer throws " + computerSelection);
+        compChoiceOutput.textContent="The computer throws " + computerSelection;
+        
 
         if (playerSelection === computerSelection){
-            return "draw"
-        }
-        return computerSelection == SELECTION_LIST[SELECTION_LIST.indexOf(playerSelection)+1] ? 
+             resultsBox.textContent="Draw!"
+        }else{resultsBox.textContent= computerSelection == SELECTION_LIST[SELECTION_LIST.indexOf(playerSelection)+1] ? 
              `You LOSE: ${computerSelection} beats ${playerSelection}!!` : 
              `You WIN: ${playerSelection} beats ${computerSelection}!!`
+        }
         
     } else {
-        console.log("Inappropriate input");
+        compChoiceOutput.textContent="You broke me and I'm not sure how..."
     }
+    if(resultsBox.textContent.includes("WIN")){
+        wins += 1;
+    }else if(resultsBox.textContent.includes("LOSE")){
+        losses += 1;
+    }
+    scoreBox.textContent=`SCORE - Player: ${wins} / COMP: ${losses}`
 }
 
 
@@ -28,3 +46,12 @@ function getComputerChoice(){
     return SELECTION_LIST[Math.floor(Math.random() * 3)];
 }
 
+rockButton.addEventListener("click", ()=>{
+    rockPaperScissors("rock");
+});
+paperButton.addEventListener("click", ()=>{
+    rockPaperScissors("paper");
+});
+scissorsButton.addEventListener("click", ()=>{
+    rockPaperScissors("paper");
+});
